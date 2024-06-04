714b33351a9abd55ac0efad8921bd4eeadec1a9f
“[B <[li.bre@northeastern.edu]>
2024-06-04 18:58:55 -0400

    simple, making sure EB is up and running

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const { DynamoDBClient, ListTablesCommand, PutItemCommand } = require("@aws-sdk/client-dynamodb");
 
+let test = 0;
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -13,8 +15,13 @@ app.get('/',(req, res) => {res.sendFile(__dirname + '/index.html');});
 app.post('/', async function(req, res) {
     let data = req.body;
     // console.log(data.name);
+	console.log(test);
     putNewItemDB(data.name, data.id, data.key, data.movie, data.review);
-    res.send(`<h1>Form submitted successfully!</h1>`);
+	console.log(test);
+	if (test==1)
+    	res.send(`<h1>Form submitted successfully!</h1>`);
+	if (test==2)
+    	res.send(`<h1>Error!</h1>`);
 });
 
 
@@ -47,10 +54,8 @@ async function putNewItemDB(name, accessKey, secretKey, title, review){
 		}
 	};
 	const command = new PutItemCommand(input);
-    try {
-        await client.send(command);
-    }
-    catch (error) {
-        console.log(error);  
-    }
+	// client.send(command);
+	await client.send(command);
+
 }
+
